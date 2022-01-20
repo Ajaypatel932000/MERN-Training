0e71bf9ccd33e15ad3d250ef9322de64940fa358
Ajaypatel932000 <ap210484@gmail.com>
2022-01-20 15:45:36 +0530

    JavaScript Done

---
+++ JavaScript/ArithFunction.js
@@ -0,0 +1,21 @@
+function Addition(a,b)
+{
+    alert("Addition is "+(a+b))
+}
+function Subtraction(a,b)
+{
+    window.alert("Subtraction is "+(a-b))
+
+}
+function Division(a,b)
+{
+    window.alert("Division is "+(a/b))
+}
+function Multiplication(a,b)
+{
+    window.alert("Multiplication is "+(a*b))
+}
+function Moduler(a,b)
+{
+    window.alert("Moduler is "+(a%b))
+}
---
+++ JavaScript/area.js
@@ -0,0 +1,16 @@
+function Circle(r)
+{
+    window.alert("Circle Area is "+(3.14*r*r))
+}
+function Rectangle(l,b)
+{
+    window.alert("Rectangle Area is "+(l*b));
+}
+function Square(l)
+{
+    window.alert("Squer area is "+(l*l))
+}
+function Triangle(l,b)
+{
+    window.alert("Triangle area is "+((l*b)/2))
+}
