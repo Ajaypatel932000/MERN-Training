704bf655017545c8292ee8bdb693606b6e898474
Ajaypatel932000 <ap210484@gmail.com>
2022-01-22 12:56:11 +0530

    jQuery done

---
+++ JavaScript/DOM/externJs.js
@@ -0,0 +1,11 @@
+function insertValue()
+{
+  
+  document.getElementById("para").innerHTML="Hello You have Clicked Button";
+  document.getElementById("para").style.backgroundColor="green";
+ 
+}
+function helloWorld()
+{
+    document.getElementById("para2").innerHTML="External JavaScript !";
+}
